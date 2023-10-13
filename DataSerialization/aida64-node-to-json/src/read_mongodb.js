6b29111bf351a68833de88f10f6a560f02255bb4
KeeneChen <keenechen@qq.com>
2023-10-13 19:15:00 +0800

    ✨ feat: 更新 aida64-node-to-json 项目

---
+++ DataSerialization/aida64-node-to-json/src/read_mongodb.js
@@ -0,0 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://keenechen:27017/mydata';
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  var dbo = db.db('mydata');
+  dbo
+    .collection('data')
+    .find({})
+    .toArray((err, result) => {
+      // 返回集合中所有数据
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+});
---
+++ DataSerialization/aida64-node-to-json/util/getAidaValues.js
@@ -0,0 +1,24 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+// 读取aida64检测数据
+async function readAida64SensorValues() {
+  const { stdout, stderr } = await exec(
+    'chcp 65001 && REG QUERY HKEY_CURRENT_USER\\Software\\FinalWire\\AIDA64\\SensorValues'
+  );
+  if (stderr) throw new Error(stderr);
+  const lines = stdout.split('\r\n').filter(line => line);
+  const sensorValues = lines.reduce((acc, cur) => {
+    const valueArr = cur.split(/\s{2,}/).filter(line => line);
+    if (valueArr.length === 3) {
+      const [type, key] = valueArr[0].split('.');
+      const value = valueArr[2];
+      if (!acc[key]) acc[key] = {};
+      acc[key][type.toLowerCase()] = value;
+    }
+    return acc;
+  }, {});
+  return sensorValues;
+}
+
+module.exports = readAida64SensorValues;
