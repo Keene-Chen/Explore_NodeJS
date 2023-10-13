9478f886023353522965a0aa4f31d8c8010ae555
KeeneChen <keenechen@qq.com>
2023-10-13 19:15:45 +0800

    ✨ feat: 更新 demo-cheerio 项目

--- DataSerialization/demo-cheerio/app.js
+++ DataSerialization/demo-cheerio/app.js
@@ -3,13 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const pretty = require('pretty');
 
-fs.readFile(
-  path.join(__dirname, './bookmarks_2023_6_17.html'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    const $ = cheerio.load(data);
-    const dtTags = $('DL DT').text(); // 选择所有<DT>标签
-    console.log(pretty(dtTags));
-  }
-);
+fs.readFile(path.join(__dirname, './bookmarks_2023_6_17.html'), 'utf-8', (err, data) => {
+  if (err) throw err;
+  const $ = cheerio.load(data);
+  const dtTags = $('DL DT').text(); // 选择所有<DT>标签
+  console.log(pretty(dtTags));
+});
