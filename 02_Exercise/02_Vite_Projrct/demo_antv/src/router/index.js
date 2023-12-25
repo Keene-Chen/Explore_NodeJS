90920825f43ea398bf2a885ed6d01da41fde0617
KeeneChen <keenechen@qq.com>
2023-12-25 21:38:41 +0800

    ✨ feat(demo_antv): 添加 Antv 图表示例

---
+++ 02_Exercise/02_Vite_Projrct/demo_antv/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@views/home/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@views/home/AboutView.vue')
+    },
+    {
+      path: '/chart',
+      name: 'chart',
+      component: () => import('@views/chart/ChartView.vue'),
+      children: [
+        {
+          path: 'bar',
+          name: 'bar',
+          component: () => import('@/views/chart/BarView.vue')
+        },
+        {
+          path: 'line',
+          name: 'line',
+          component: () => import('@/views/chart/LineView.vue')
+        },
+        {
+          path: 'pie',
+          name: 'pie',
+          component: () => import('@/views/chart/PieView.vue')
+        }
+      ]
+    }
+  ]
+});
+
+export default router;
---
+++ 02_Exercise/02_Vite_Projrct/demo_antv/src/views/chart/ChartView.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <RouterLink to="/chart">Chart</RouterLink>
+    <RouterLink to="/chart/bar">Bar</RouterLink>
+    <RouterLink to="/chart/line">line</RouterLink>
+    <RouterLink to="/chart/pie">pie</RouterLink>
+  </nav>
+  <RouterView></RouterView>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style scoped></style>
