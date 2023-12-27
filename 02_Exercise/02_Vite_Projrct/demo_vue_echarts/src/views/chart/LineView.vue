10f8b2cd6c4a9fca1275dbf086aad9dcc6c62020
KeeneChen <keenechen@qq.com>
2023-12-27 21:49:21 +0800

    ✨ feat(demo_vue_echarts): 添加 vue-echarts 图表示例

---
+++ 02_Exercise/02_Vite_Projrct/demo_vue_echarts/src/views/chart/LineView.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>line</span>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.chart {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
