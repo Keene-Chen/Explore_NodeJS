147f5305ea471d7ca20248391c2a6075e7584953
KeeneChen <keenechen@qq.com>
2024-05-26 02:04:47 +0800

    feat: 添加 git commit 提交信息验证脚本配置

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,9 @@
-import { pico, run } from './utils/run.js';
+import { pico, run } from "./utils.js";
 
 (async () => {
-  const res = await run('rimraf', ['-g', '**/dist', '**/node_modules']);
-  console.log(pico.bgRed(pico.white(`${res.durationMs}ms`)), pico.blue(`${res.escapedCommand}`));
+  const res = await run("rimraf", ["-g", "**/dist", "**/node_modules"]);
+  console.log(
+    pico.bgRed(pico.white(`${res.durationMs}ms`)),
+    pico.blue(`${res.escapedCommand}`),
+  );
 })();
